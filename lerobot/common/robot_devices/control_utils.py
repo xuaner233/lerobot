f8afa9384a3d22c1439d3d44d72cae92495a9f07
Xiaoxuan Liu <xiaoxuan.liu@corelabtech.com>
2025-03-06 17:42:19 +0800

    Reduce control log to per 5 sec

--- lerobot/common/robot_devices/control_utils.py
+++ lerobot/common/robot_devices/control_utils.py
@@ -266,8 +266,9 @@ def control_loop(
             dt_s = time.perf_counter() - start_loop_t
             busy_wait(1 / fps - dt_s)
 
-        dt_s = time.perf_counter() - start_loop_t
-        log_control_info(robot, dt_s, fps=fps)
+        if (timestamp % 5 < 0.01):
+            dt_s = time.perf_counter() - start_loop_t
+            log_control_info(robot, dt_s, fps=fps)
 
         timestamp = time.perf_counter() - start_episode_t
         if events["exit_early"]:
